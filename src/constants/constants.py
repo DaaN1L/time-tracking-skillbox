8adf8398be432333e98f3faa040aa2b751697978
DaaN1L <dorofeev-danila@mail.ru>
2024-11-29 19:23:07 +0300

    fix: move path stem of imported files to constansts

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -1,7 +1,9 @@
-csv_schemas = {
-    "employees": ("employee_full_name", "position_nm"),
-    "positions": ("position_nm", "hh_billing_rate"),
-    "timesheet": ("task_cd", "employee_full_name", "task_start_dttm", "task_end_dttm"),
-}
 employee_path_stem = "employees"
 position_path_stem = "positions"
+timesheet_path_stem = "timesheet"
+
+csv_schemas = {
+    employee_path_stem: ("employee_full_name", "position_nm"),
+    position_path_stem: ("position_nm", "hh_billing_rate"),
+    timesheet_path_stem: ("task_cd", "employee_full_name", "task_start_dttm", "task_end_dttm"),
+}
